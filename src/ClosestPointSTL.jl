d57677c75b5da98b90a80b9fbeaf383bdf9c9d0c
Jan Modderman <93585667+janmodderman@users.noreply.github.com>
2024-04-29 09:42:34 +0200

    Update ClosestPointSTL.jl
    fix packages

--- src/ClosestPointSTL.jl
+++ src/ClosestPointSTL.jl
@@ -1,18 +1,21 @@
 module ClosestPointSTL
 
-using MeshIO
-using FileIO
-using GeometryBasics
-using Distances
-using LinearAlgebra
 using BoundingVolumeHierarchies
 import BoundingVolumeHierarchies: PointT, _normal, children, FaceIndices
+using DataStructures
+using Distances
+using FileIO
+using GeometryBasics
+using MeshIO
+using Nearestneighbors
+using Rotations
+using StaticArrays
+using TimerOutputs
 
 export closest_point_projection
 export intersects
 
 include("Intersections.jl")
 include("Closest_point.jl")
-
 
 end
